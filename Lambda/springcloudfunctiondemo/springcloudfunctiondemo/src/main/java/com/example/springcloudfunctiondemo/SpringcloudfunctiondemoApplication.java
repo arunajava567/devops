b1881b7fae2509c458a2c8060aad4eb67cb9acda
arunajava567 <30722352+arunajava567@users.noreply.github.com>
2021-11-19 17:29:35 +0530

    Add files via upload

---
+++ Lambda/springcloudfunctiondemo/springcloudfunctiondemo/src/main/java/com/example/springcloudfunctiondemo/SpringcloudfunctiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudfunctiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudfunctiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudfunctiondemoApplication.class, args);
+	}
+
+}
